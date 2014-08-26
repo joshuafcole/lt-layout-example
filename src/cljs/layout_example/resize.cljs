9b855720800b1acf445618460a7229f2c1fad857
Joshua Cole <joshuafcole@gmail.com>
2014-08-26 09:11:27 -0700

    Abstracts real-width and real-height

--- src/cljs/layout_example/resize.cljs
+++ src/cljs/layout_example/resize.cljs
@@ -1,35 +1,43 @@
 (ns lt.layout.resize
   (:require [jayq.core :as jayq]))
 
+(defn real-width
+  ([$el val] (.css $el "width" (str val "px")))
+  ([$el] (js/parseFloat (.css $el "width"))))
+
+(defn real-height
+  ([$el val] (.css $el "height" (str val "px")))
+  ([$el] (js/parseFloat (.css $el "height"))))
+
 (defn left [$resizable event]
   (let [$target (jayq/$ (aget event "target"))
         target-x (aget (.offset $target) "left")
         origin-x (+ (aget (.offset $resizable) "left")
-                    (js/parseFloat (.css $resizable "width")))
+                    (real-width $resizable))
         off-x (- origin-x (+ target-x (.-offsetX event)))]
-    (.css $resizable "width" (str off-x "px"))))
+    (real-width $resizable off-x)))
 
 (defn right [$resizable event]
   (let [$target (jayq/$ (aget event "target"))
         target-x (aget (.offset $target) "left")
         origin-x (aget (.offset $resizable) "left")
         off-x (- (+ target-x (.-offsetX event)) origin-x)]
-    (.css $resizable "width" (str off-x "px"))))
+    (real-width $resizable off-x)))
 
 (defn top [$resizable event]
   (let [$target (jayq/$ (aget event "target"))
         target-y (aget (.offset $target) "top")
         origin-y (+ (aget (.offset $resizable) "top")
-                    (js/parseFloat (.css $resizable "height")))
+                    (real-height $resizable))
         off-y (- origin-y (+ target-y (.-offsetY event)))]
-    (.css $resizable "height" (str off-y "px"))))
+    (real-height $resizable off-y)))
 
 (defn bottom [$resizable event]
   (let [$target (jayq/$ (aget event "target"))
         target-y (aget (.offset $target) "bottom")
         origin-y (aget (.offset $resizable) "bottom")
         off-y (- (+ target-y (.-offsetY event)) origin-y)]
-    (.css $resizable "height" (str off-y "px"))))
+    (real-height $resizable off-y)))
 
 
 (defn start [resize $resizable]
