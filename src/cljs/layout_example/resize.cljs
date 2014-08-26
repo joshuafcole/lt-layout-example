f9735996bfab0067bcef33c944070a78b37e78e4
Joshua Cole <joshuafcole@gmail.com>
2014-08-26 09:17:44 -0700

    Simplifies resize calculation

--- src/cljs/layout_example/resize.cljs
+++ src/cljs/layout_example/resize.cljs
@@ -10,33 +10,25 @@
   ([$el] (js/parseFloat (.css $el "height"))))
 
 (defn left [$resizable event]
-  (let [$target (jayq/$ (aget event "target"))
-        target-x (aget (.offset $target) "left")
-        origin-x (+ (aget (.offset $resizable) "left")
+  (let [origin-x (+ (aget (.offset $resizable) "left")
                     (real-width $resizable))
-        off-x (- origin-x (+ target-x (.-offsetX event)))]
+        off-x (- origin-x (.-pageX event))]
     (real-width $resizable off-x)))
 
 (defn right [$resizable event]
-  (let [$target (jayq/$ (aget event "target"))
-        target-x (aget (.offset $target) "left")
-        origin-x (aget (.offset $resizable) "left")
-        off-x (- (+ target-x (.-offsetX event)) origin-x)]
+  (let [origin-x (aget (.offset $resizable) "left")
+        off-x (- (.-pageX event) origin-x)]
     (real-width $resizable off-x)))
 
 (defn top [$resizable event]
-  (let [$target (jayq/$ (aget event "target"))
-        target-y (aget (.offset $target) "top")
-        origin-y (+ (aget (.offset $resizable) "top")
+  (let [origin-y (+ (aget (.offset $resizable) "top")
                     (real-height $resizable))
-        off-y (- origin-y (+ target-y (.-offsetY event)))]
+        off-y (- origin-y (.-pageY event))]
     (real-height $resizable off-y)))
 
 (defn bottom [$resizable event]
-  (let [$target (jayq/$ (aget event "target"))
-        target-y (aget (.offset $target) "bottom")
-        origin-y (aget (.offset $resizable) "bottom")
-        off-y (- (+ target-y (.-offsetY event)) origin-y)]
+  (let [origin-y (aget (.offset $resizable) "bottom")
+        off-y (- (.-pageY event) origin-y)]
     (real-height $resizable off-y)))
 
 
